6c75bbee6e11cc1bf6027bbf905ddb277e5bf243
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-16 20:09:55 -0300

    search a la mitad y __v

--- archivos/data/farmacia.js
+++ archivos/data/farmacia.js
@@ -20,23 +20,26 @@ console.log(carrito)
   const seccionFarmacia = document.getElementById(`sectionFarmacia`)
   seccionFarmacia.innerHTML = crearFarmacia
   function crearMasFarmacia(farmaciaCarta) {
-    let unidadesTexto = "Unidades disponibles";
-    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    
- 
-    for (let item of carrito) {
-      if (item.producto === farmaciaCarta.producto) {
-        farmaciaCarta.disponibles -= 1;
-      }
+  let unidadesTexto = "Unidades disponibles";
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  
+  // crear copia
+  const farmaciaCartaCopy = { ...farmaciaCarta };
+  
+  for (let item of carrito) {
+    if (item.producto === farmaciaCartaCopy.producto) {
+      farmaciaCartaCopy.disponibles -= 1;
     }
-    
-    if (farmaciaCarta.disponibles <= 0) {
-      unidadesTexto = "Sin unidades";
-    } else if (farmaciaCarta.disponibles === 1) {
-      unidadesTexto = "Última unidad disponible";
-    } else if (farmaciaCarta.disponibles <= 3) {
-      unidadesTexto = `Últimas ${farmaciaCarta.disponibles} unidades`;
-    }
+  }
+  
+  if (farmaciaCartaCopy.disponibles <= 0) {
+    unidadesTexto = "Sin unidades";
+  } else if (farmaciaCartaCopy.disponibles === 1) {
+    unidadesTexto = "Última unidad disponible";
+  } else if (farmaciaCartaCopy.disponibles <= 3) {
+    unidadesTexto = `Últimas ${farmaciaCartaCopy.disponibles} unidades`;
+  }
+  
   
     return `
       <div class="card carta-farmacia"" style="width: 18rem;">
@@ -61,11 +64,20 @@ console.log(carrito)
       if (item.disponibles > 0) {
         carrito.push(item);
         item.disponibles--;
+        item.__v++;
+
         actualizarUnidades(item);
         //paso el array a string
        /*  const json = JSON.stringify(carrito) */
        
        localStorage.setItem("carrito", JSON.stringify(carrito));
+       Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'tu compra se añadio al carro',
+        showConfirmButton: false,
+        timer: 1500
+      })
       }
     })
     })
@@ -112,9 +124,75 @@ function actualizarUnidades(item) {
       } else if(farmaciaCarta.disponibles <= 3 && farmaciaCarta.disponibles > 1) {
         unidadesTexto.textContent = `últimas ${item.disponibles} unidades`;
       }
+    
     }
   });
 }
+const barraDeBusquedaValor = document.getElementById('search-input');
+barraDeBusquedaValor.addEventListener('input', () => {
+  const  busquedaValor = barraDeBusquedaValor.value.toLowerCase();
+  const  cartasFarmaciaFiltradas = farmaciaPetShop.filter((farmaciaPetShop) =>{
+   return farmaciaPetShop.producto.toLowerCase().includes(busquedaValor);
+  
+  });
+  const seccionFarmacia = document.getElementById(`sectionFarmacia`)
+  seccionFarmacia.innerHTML = '';
+  for (let i = 0; i <  cartasFarmaciaFiltradas.length; i++) {
+    seccionFarmacia.innerHTML += crearMasFarmacia( cartasFarmaciaFiltradas[i]);
+  }
+  
+  const cartasFarmacia = document.querySelectorAll('.carta-farmacia');
+  cartasFarmacia.forEach(carta => {
+    const botonAnadir = carta.querySelector('.buttonAnadir');
+    botonAnadir.addEventListener('click', (event) => {
+      event.stopPropagation();
+      const producto = event.target.getAttribute('data-producto');
+      const item = farmaciaPetShop.find(objeto => objeto.producto === producto);
+      if (item.disponibles > 0) {
+        carrito.push(item);
+        item.disponibles--;
+        actualizarUnidades(item);
+        //paso el array a string
+       /*  const json = JSON.stringify(carrito) */
+       
+       localStorage.setItem("carrito", JSON.stringify(carrito));
+       Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      }
+    })
+    })
+    cartasFarmacia.forEach(carta => {
+      carta.addEventListener('click', () => {
+        const modalFarmacia = document.getElementById("modalFarmacia");
+        const modalTitulo = document.getElementById("modal-titulo");
+        const modalImagen = document.getElementById("modal-imagen");
+        const modalDescripcion = document.getElementById("modal-descripcion");
+        const modalPrecio = document.getElementById("modal-precio");
+        const modalUnidades = document.getElementById("modal-unidades");
+        const farmaciaCarta = farmaciaPetShop.find(objeto => objeto.producto === carta.querySelector(".card-title").textContent);
+    
+        modalTitulo.textContent = farmaciaCarta.producto;
+        modalImagen.src = farmaciaCarta.imagen;
+        modalDescripcion.textContent = farmaciaCarta.descripcion;
+        modalPrecio.textContent = `Precio: $${farmaciaCarta.precio}`;
+        modalUnidades.textContent = `Unidades: ${farmaciaCarta.disponibles}`;
+    
+        modalFarmacia.style.display = "block";
+      });
+    });
+    const cerrarModal = document.querySelector('.close');
+    const modalFarmacia = document.getElementById("modalFarmacia");
+    
+    cerrarModal.addEventListener('click', () => {
+      modalFarmacia.style.display = "none";
+    });
+    
+})
 
 })
 .catch(error => console.log(error));
